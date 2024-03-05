e0048c278e264f9ab3123f63fc6ea04e306fd38f
Karl Mathias Moberg <km@kmoberg.no>
2024-03-05 23:53:35 +0100

    fix: Make sure to add the event trigger

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -58,7 +58,7 @@ resource "aws_s3_bucket_notification" "lambda_thumbgenerator_bucket_notification
 
   lambda_function {
     lambda_function_arn = aws_lambda_function.thumbgenerator_lambda.arn
-    events              = ["s3:ObjectPut:*"]
+    events              = ["s3:ObjectCreated:Put:*"]
     filter_prefix       = "static/uploads/"
     filter_suffix       = ".jpg"
   }
